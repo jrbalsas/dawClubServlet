837d7f92c37d094bf4739dd7c9f2267e2935e232
jrbalsas@ujaen.es <>
2014-03-20 19:23:27 +0100

    - Change location package for DAOs

--- src/java/daw/club/model/dao/ClienteDAO.java
+++ src/java/daw/club/model/dao/ClienteDAO.java
@@ -1,6 +1,7 @@
 package daw.club.model.dao;
 
 import daw.club.model.Cliente;
+import daw.club.model.Cliente;
 import java.util.List;
 
 public interface ClienteDAO extends GenericDAO<Cliente,Integer>{
--- src/java/daw/club/model/dao/ClienteDAOJDBC.java
+++ src/java/daw/club/model/dao/ClienteDAOJDBC.java
@@ -1,6 +1,7 @@
 package daw.club.model.dao;
 
 import daw.club.model.Cliente;
+import daw.club.model.Cliente;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
